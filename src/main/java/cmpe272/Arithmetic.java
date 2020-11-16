d6017e16339286a49a23be6e4b826afd7bbdd430
tamannamehta <67537634+tamannamehta@users.noreply.github.com>
2020-11-15 22:19:23 -0800

    Break the code

--- src/main/java/cmpe272/Arithmetic.java
+++ src/main/java/cmpe272/Arithmetic.java
@@ -5,7 +5,7 @@ package cmpe272;
  */
 public class Arithmetic {
     public int add(int a, int b) {
-        return (a + b);
+        return (a * b);
     }
 
     public int subtract(int a, int b) {
